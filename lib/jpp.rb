d4b26a02142010aef8bfa42043a8ee271d85b8a9
Benton Porter <benton.porter@bazaarvoice.com>
2013-02-08 16:42:56 -0600

    Add --indent option

--- lib/jpp.rb
+++ lib/jpp.rb
@@ -1,18 +1,26 @@
 require 'json'
 require 'optparse'
+require 'ostruct'
 require 'hash'
 
 class Jpp
   VERSION = '0.2'
 
   def main()
-    options = {}
+    # Set default values
+    options = OpenStruct.new
+    options.indent = 2
+    options.sort = false
 
     parser = OptionParser.new do |opts|
       opts.banner = 'Usage: jpp [options] file'
 
+      opts.on('-i', '--indent N', Integer, "Number of spaces to indent (default: #{options.indent}) ") do |i|
+        options.indent = i
+      end
+
       opts.on('-s', '--sort', 'Sort json alphabetically by key') do |s|
-        options[:sort] = s
+        options.sort = s
       end
 
       opts.on('-v', '--version', 'Show version') do |v|
@@ -22,15 +30,15 @@ class Jpp
     end
 
     begin parser.parse!
-    rescue OptionParser::InvalidOption => message
+    rescue OptionParser::ParseError => message
       puts message
       puts parser
       exit 1
     end
 
     input = JSON.load(ARGF.read)
-    input = input.sort_by_key() if options[:sort]
-    output = JSON.pretty_generate(input)
+    input = input.sort_by_key() if options.sort
+    output = JSON.pretty_generate(input, :indent => ' ' * options.indent)
     puts output
   end
 end
