bfae89aa239713d0d230850aa4f1ed9315977368
Benton Porter <benton.porter@bazaarvoice.com>
2013-02-17 21:01:16 -0600

    Support printing out arrays

--- lib/jpp.rb
+++ lib/jpp.rb
@@ -13,7 +13,7 @@ module Jpp
       end
 
       input = JSON.load(ARGF.read)
-      input = input.sort_by_key() if options.sort  # TODO make sure input is a hash before calling sort_by_key
+      input = input.sort_by_key() if options.sort and input.is_a?(Hash)       # TODO support sorting hashes within an array
       output = JSON.pretty_generate(input, :indent => ' ' * options.indent)
       puts output
     end
